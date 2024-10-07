2c82c3489c7b08902d77e4b2125b8c0a03602934
gudzsv <gudzsvmail@gmail.com>
2024-10-07 20:36:55 +0300

    added active and not active label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import ActiveLabel from '@/app/components//active-label';
+import NotActiveLabel from '@/app/components/not-active-label';
+
 export default function Home() {
-	return (
-		<>
-			<main>
-				<h1>Home Page</h1>
-			</main>
-			<footer>
-				<p>footer</p>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <main>
+        <h1>Home Page</h1>
+        <ActiveLabel>Active</ActiveLabel>
+        <NotActiveLabel>Not Active</NotActiveLabel>
+      </main>
+      <footer>
+        <p>footer</p>
+      </footer>
+    </>
+  );
 }
