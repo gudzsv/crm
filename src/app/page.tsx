7d6f643fce7f0bf55de695ee9081c06b0a31be67
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-10-28 12:28:03 +0100

    test dynamic component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import StatusLabel, { Status } from '@/app/components/satatus-label';
+import { headers } from 'next/headers.js';
 
 export default function Home() {
+  console.log(headers());
   return (
     <>
       <main>
-        <h1 className="text-xl">Home Page</h1>
+        <h1 className="text-xl">Home Page {new Date().toTimeString()}</h1>
         <StatusLabel status={Status.Active}>Active</StatusLabel>
         <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
         <StatusLabel status={Status.Pending}>Pending</StatusLabel>
