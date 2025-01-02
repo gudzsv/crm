a5465de8d34461638eecd97ccac8d683f364d57f
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-02 17:27:44 +0100

    add btn to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleMoveToCRM = () => {
+    router.push('/dashboard');
+  };
   return (
-    <main>
-      <h1 className="text-xl">Home page</h1>
+    <main className="text-center">
+      <h1 className="text-xxl mb-2">Home Page</h1>
+      <hr />
+      <button
+        onClick={handleMoveToCRM}
+        type="button"
+        className="py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded hover:bg-gray-800 active:bg-gray-950 mt-2"
+      >
+        Move to CRM
+      </button>
     </main>
   );
 }
