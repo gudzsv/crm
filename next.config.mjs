6bbce1464c31c8ca637eb00906ee2433976d598d
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-02 16:05:50 +0100

    remive comments in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// export default nextConfig;
-
-// const nextConfig = {
-//   experimental: {
-//     optimizeCss: true,
-//   },
-// };
-
 export default nextConfig;
